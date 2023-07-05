8b1208cce358e430bf67c7dba1a35ff66ca88992
Sherif Alsayed <sherif@Sherifs-MacBook-Pro.local>
2023-07-05 23:16:33 +0200

    cleanup

--- freerasp_tool/lib/home_page.dart
+++ freerasp_tool/lib/home_page.dart
@@ -10,9 +10,8 @@ class HomePage extends HookWidget {
     final threatState = useState('');
     useEffect(() {
       Future.microtask(() async {
-        /// add your key hash
         final base64Hash = hashConverter.fromSha256toBase64(
-            '');
+            '6F:B1:3E:85:9E:B8:4E:18:BE:50:A4:50:E6:A7:7E:8B:F1:FB:44:A2:E5:AD:9A:6D:E0:FB:1D:60:1A:88:F7:6B');
         final config = TalsecConfig(
           /// For Android
           androidConfig: AndroidConfig(
@@ -21,19 +20,17 @@ class HomePage extends HookWidget {
           ),
 
           /// For iOS
-          /// teamid and watcher to send threat report to it
           iosConfig: IOSConfig(
             bundleIds: ['com.demos.freerasp_tool'],
-            teamId: '',
+            teamId: 'D2A8ZV8F9B',
           ),
-          watcherMail: '',
+          watcherMail: 'sherif.hasan1994@gmail.com',
         );
         await Talsec.instance.start(config);
         Talsec.instance.onThreatDetected.listen((threat) {
           threatState.value = 'Threat detected: $threat';
         });
       });
-
       return null;
     });
 
--- mapbox_example/lib/app_constants.dart
+++ mapbox_example/lib/app_constants.dart
@@ -1,11 +1,12 @@
 import 'package:latlong2/latlong.dart';
 
 class AppConstants {
-  // Add your data to be able to use the sample
-  static const mapBoxUrlTemplate = '';
-  static const String mapBoxAccessToken = '';
+  static const mapBoxUrlTemplate =
+      'https://api.mapbox.com/styles/v1/sherifalsayed/{mapStyleId}/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}';
+  static const String mapBoxAccessToken =
+      'pk.eyJ1Ijoic2hlcmlmYWxzYXllZCIsImEiOiJjbGdxeXNtdzMxZWhmM2VtbTRsdW8xZmJiIn0.CtX0zolXMYV0DsJ6tZIQXQ';
 
-  static const String mapBoxStyleId = '';
+  static const String mapBoxStyleId = 'clgqz2puy000601r0gxkl1jpw';
 
   static final startLocation = LatLng(48.1481406, 11.4612251);
   static const dataJsonPath = 'assets/data.json';
